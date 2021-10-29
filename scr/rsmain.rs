a52d25a7c111cea34829e8e1cdc7d014b4687606
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-29 15:20:18 -0400

    Update rsmain.rs

--- scr/rsmain.rs
+++ scr/rsmain.rs
@@ -1,36 +1,25 @@
-use std::fs::{File, OpenOptions};
-use std::os::unix::{fs::OpenOptionsExt, io::{RawFd, FromRawFd, IntoRawFd}};
-use std::path::Path;
-
-use input::{Libinput, LibinputInterface};
-use libc::{O_RDONLY, O_RDWR, O_WRONLY};
-
-struct Interface;
-
-impl LibinputInterface for Interface {
-    fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<RawFd, i32> {
-        OpenOptions::new()
-            .custom_flags(flags)
-            .read((flags & O_RDONLY != 0) | (flags & O_RDWR != 0))
-            .write((flags & O_WRONLY != 0) | (flags & O_RDWR != 0))
-            .open(path)
-            .map(|file| file.into_raw_fd())
-            .map_err(|err| err.raw_os_error().unwrap())
-    }
-    fn close_restricted(&mut self, fd: RawFd) {
-        unsafe {
-            File::from_raw_fd(fd);
-        }
-    }
-}
+use std::fs;
 
 fn main() {
-    let mut input = Libinput::new_with_udev(Interface);
-    input.udev_assign_seat("seat0").unwrap();
-    loop {
-        input.dispatch().unwrap();
-        for event in &mut input {
-            println!("Got event: {:?}", event);
+    let data = fs::read_to_string("config.set").expect("Unable to read file!");
+    let fsread = data;
+    let mut f = BufReader::new(File::open(fsread).expect("open failed"));
+    let mut stringset;
+    let mut cmda;
+    let mut auga;
+    let mut augb;
+    let mut augset;
+    for line in f.lines() {
+        for c in line.expect("lines failed").chars() {
+            if c != "." {
+                stringset = concat!(stringset, c).expect(".CONCAT of file failed...");
+                cmda = stringset;
+            } else {
+                if c != "." {
+                    augset = concat!(stringset, c).expect(".CONCAT of aug failed");
+                }
+            }
         }
     }
 }
+
