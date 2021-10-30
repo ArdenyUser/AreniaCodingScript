4549bdd1e644f614877cb468a5b2ade780e2f51e
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-30 13:20:57 -0400

    Update rsmain.rs

--- scr/rsmain.rs
+++ scr/rsmain.rs
@@ -1,7 +1,6 @@
 use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::string;
 use std::prelude::rust_2021::String as String;
 
 
@@ -9,12 +8,14 @@ pub fn main() {
     let data = fs::read_to_string("config.set");
     let fsread = data;
     let mut stringset:String;
+	let mut stringset = String::new();
     let mut auga:String;
     let mut augb:String;
     let mut setting;
 	let mut stringsetaug:String;
+	let mut stringsetaug = String::new();
     setting = "B";
-	let mut f = BufReader::new(File::open("main.fs").expect("open failed"));
+	let f = BufReader::new(File::open("main.fs").expect("open failed"));
     for line in f.lines() {
         for c in line.expect("lines failed").chars() {
             // main system
