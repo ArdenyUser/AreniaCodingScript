5e2732e0d77277b72fd5d460b3572b50df6783a5
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-30 09:18:38 -0400

    Update rsmain.rs

--- scr/rsmain.rs
+++ scr/rsmain.rs
@@ -1,28 +1,27 @@
 use std::fs;
 use std::File;
-use std::io::BufReader;
-use std::io::BufRead;
 
 fn main() {
     let data = fs::read_to_string("config.set").expect("Unable to read file!");
     let fsread = data;
-    let mut f = BufReader::new(File::open(fsread).expect("open failed"));
+    let filedat = fs::read_to_string(fsread).expect("Unable to read file!");
     let mut stringset;
     let mut cmda;
     let mut auga;
     let mut augb;
     let mut augset;
     let mut setting;
-    for line in f.lines() {
+    setting = "B";
+    for line in filedat.lines() {
         for c in line.expect("lines failed").chars() {
             if c != " " && setting != "A" {
-                stringset = [stringset, c].join().expect(".CONCAT of file failed...");
+                stringset = [stringset, c].expect(".CONCAT of file failed...");
                 cmda = stringset;
             } else {
                 setting = "A";
-                if c != ";" {
-                    stringset
-                    augset = [stringset, c].join().expect(".CONCAT of file failed...");
+                if c != ";" && setting == "A" {
+                    stringset = " ";
+                    augset = [stringset, c].expect(".CONCAT of file failed...");
                 }
             }
         }
